02480abedeae21899346ea7d44ecf5eff68d9e35
heeeunk <heeeunk777@gmail.com>
2021-11-03 02:07:51 +0900

    feat: build project

---
+++ client/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>home</div>
+    <div>aflaksfjkasflsjlkf</div>
+    <button @click="get">글가져오기</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  methods: {
+    get: function(){
+      axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/api/v1/articles',
+      })
+      .then(res => {
+        console.log(res)
+
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
